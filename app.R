c59beb1b0d983cfad94653fab4fc8f90469238be
nthongle317 <64118033+nthongle317@users.noreply.github.com>
2023-02-26 20:09:16 +0700

    Add files via upload

---
+++ app.R
@@ -0,0 +1,86 @@
+library(shiny)
+library(DT)
+source("/media/thong/sda/RCID/CoHoai/Tuberculosis/ShinyR/config/appDevelop.R")
+
+ui <- fluidPage(
+  titlePanel("Research center for infectious disease - RCID"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Select model and machine learning algorithm"),
+      selectInput("var1", 
+        label = "Choose Model",
+        choices = list("4-CpGs model", "5-CpGs model", "8-CpGs model", "12-CpGs model"), 
+        selected = "8-CpGs model"),
+      selectInput("var2", 
+        label = "Choose Algorithm",
+        choices = list("Logistic regression", "SVM", "Random forest", "KNN", "Adaboost"), 
+        selected = "Logistic regression"),
+      helpText("Now, upload your CpG methylation level here"),
+      fileInput("file1", "Choose CSV File", 
+        accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")
+        ),
+      checkboxInput("header", "Header", TRUE)
+      ),
+    mainPanel(
+      tags$head(
+        tags$style(HTML("
+          .sep {
+            width: 20px; height: 1px; float: left;
+          }
+          "))
+        ),
+      h1("Shiny App for using DNA methylation predict TB condition"),
+      textOutput("var1"),
+      textOutput("var2"),
+      DT::dataTableOutput("tablePred"),
+      h3("Citation: "),
+      h4("For more Information about RCID: ")
+      )
+    )
+  )
+
+# Define server logic ----
+server <- function(input, output) {
+  output$var1 <- renderText({ 
+    paste("You have selected", input$var1)
+    })
+  output$var2 <- renderText({ 
+  paste("You have selected", input$var2)
+  })
+  output$tablePred=DT::renderDataTable ({
+    x <- switch(input$var1, 
+      "4-CpGs model" = "LASSO", 
+      "5-CpGs model" = "MUVR_PLS",
+      "8-CpGs model" = "BMA",
+      "12-CpGs model" = "MUVR_rf")
+    y <- switch(input$var2, 
+      "Logistic regression" = "logit", 
+      "SVM" = "svm",
+      "Random forest" = "rf",
+      "KNN" = "knn",
+      "Adaboost" = "adaboost")
+    file <- input$file1
+    ext <- tools::file_ext(file$datapath)
+    req(file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    valDT=read.csv(file$datapath, header = input$header)
+    my_data=app(x, y, valDT)
+    DT::datatable(my_data, 
+      extensions = "Buttons",
+      options = list(
+        paging = TRUE,
+        rownames = FALSE,
+        scrollX=TRUE,
+        searching = TRUE,
+        ordering = TRUE,
+        dom = 'l<"sep">Bfrtip',
+        buttons = c('copy', 'csv', 'excel'),
+        pageLength=10,
+        lengthMenu=c(10,20,50,100)
+        )
+      )
+    })
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)  
---
+++ config/appDevelop.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(pROC)
+library(caret)
+
+app = function(model, algorithm, valDT) {
+
+# valDT=read.table("/media/thong/sda/RCID/CoHoai/Tuberculosis/ShinyR/model/example.csv", header=T, sep=",")
+rownames(valDT)=valDT$Sample
+dt=valDT[,grep("cg", names(valDT))]
+
+CpG=read.table(paste0("/media/thong/sda/RCID/CoHoai/Tuberculosis/ShinyR/model/", model, ".txt"), header=T, sep="\t")$CpGs
+
+REQ=data.frame(Required=CpG)
+UI=data.frame(RequiredInput=names(dt), UserInput=names(dt))
+
+check=merge(REQ, UI, by.x="Required", by.y="RequiredInput", all.x=T)
+n=sum(is.na(check$UserInput))
+
+if (n>0) {
+check[is.na(check$UserInput), "UserInput"] = "Missing value in this CpG, check your file"
+print(check)
+} else if (n==0) {
+
+select=paste0(model, "_", algorithm)
+
+rf.m1=readRDS(paste0("/media/thong/sda/RCID/CoHoai/Tuberculosis/ShinyR/model/", select, ".rds"))
+
+pre.m1s <- predict(rf.m1, dt, type="prob")
+
+pre.m1s$predict <- factor(ifelse(pre.m1s$HC >= .5, "Normal", "TB"))
+
+outDF=cbind(pre.m1s, dt)
+outDF$Sample=rownames(outDF)
+
+outDF=outDF[,c("Sample", "predict", CpG)]
+rownames(outDF)=NULL
+print(outDF)
+
+}
+
+}
